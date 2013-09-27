157006cf90ca05984174060028c133d7a7fdd952
Warbossrus26@gmail.com <Warbossrus26@gmail.com@bb2aebea-ba2d-cad3-75da-7cf554a62b86>
2013-09-27 13:47:18 +0000

    #3 Another rendering changes. Slow model parsing, more memory consume but fast drawing.

---  course-work-ps-31-2013 --username dertkaes@gmail.com/OpenGLTest/OpenGLTest/3dModel.cpp
+++  course-work-ps-31-2013 --username dertkaes@gmail.com/OpenGLTest/OpenGLTest/3dModel.cpp
@@ -3,16 +3,18 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-//#define NEW_RENDERING //uncomment it to render normals and texture coords in exchange of speed
 
 C3DModel::C3DModel(std::string const& path)
 {
+	std::vector<double> vertices;
+	std::vector<double> textureCoords;
+	std::vector<double> normals;
 	std::ifstream iFile;
 	iFile.open(path);
 	std::string line;
 	std::string type;
 	double dvalue;
-	unsigned int uintvalue;
+	bool useFaces = false;
 	while(std::getline(iFile, line))
 	{
 		if(line.empty() || line[0] == '#')//Empty line or commentary
@@ -27,7 +29,7 @@ C3DModel::C3DModel(std::string const& path)
 			{
 				dvalue = 0.0;
 				lineStream >> dvalue;
-				m_vertices.push_back(dvalue);
+				vertices.push_back(dvalue);
 			}
 		}
 
@@ -37,7 +39,7 @@ C3DModel::C3DModel(std::string const& path)
 			{
 				dvalue = 0.0;
 				lineStream >> dvalue;
-				m_textureCoords.push_back(dvalue);
+				textureCoords.push_back(dvalue);
 			}
 		}
 		if(type == "vn")// Normals
@@ -46,38 +48,56 @@ C3DModel::C3DModel(std::string const& path)
 			{
 				dvalue = 0.0;
 				lineStream >> dvalue;
-				m_normals.push_back(dvalue);
+				normals.push_back(dvalue);
 			}
 		}
 		if(type == "f")// faces
 		{
+			useFaces = true;
 			for(unsigned int i = 0; i < 3; ++i)
 			{
 				std::string indexes;
 				lineStream >> indexes;
 				std::stringstream indexStream(indexes);
 				std::string index;
+				unsigned int vertexIndex;
+				unsigned int textureIndex;
+				unsigned int normalIndex;
 				//vertex index
 				std::getline(indexStream, index, '/');
-				uintvalue = atoi(index.c_str());
-				--uintvalue;
-				m_faces.push_back(uintvalue);
-#ifdef NEW_RENDERING
+				vertexIndex = (atoi(index.c_str()) - 1) * 3;
+				m_vertices.push_back(vertices[vertexIndex]);
+				m_vertices.push_back(vertices[vertexIndex + 1]);
+				m_vertices.push_back(vertices[vertexIndex + 2]);
+				index.clear();
 				//texture coord index.
 				std::getline(indexStream, index, '/');
-				if(!index.empty()) uintvalue = atoi(index.c_str());
-				--uintvalue;
-				m_faces.push_back(uintvalue);
+				if(!index.empty())
+				{
+					textureIndex = (atoi(index.c_str()) - 1) * 2;
+					m_textureCoords.push_back(textureCoords[textureIndex]);
+					m_textureCoords.push_back(textureCoords[textureIndex + 1]);
+				}
+				index.clear();
 				//normal index
 				std::getline(indexStream, index);
-				if(!index.empty()) uintvalue = atoi(index.c_str());
-				--uintvalue;
-				m_faces.push_back(uintvalue);/**/
-#endif
+				if(!index.empty())
+				{
+					normalIndex = (atoi(index.c_str()) - 1) * 3;
+					m_normals.push_back(normals[normalIndex]);
+					m_normals.push_back(normals[normalIndex + 1]);
+					m_normals.push_back(normals[normalIndex + 2]);
+				}
 			}
 		}
 	}
 	iFile.close();
+	if(!useFaces)
+	{
+		m_vertices = vertices;
+		m_textureCoords = textureCoords;
+		m_normals = normals;
+	}
 }
 
 void C3DModel::Draw()
@@ -97,25 +117,7 @@ void C3DModel::Draw()
 		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 		glTexCoordPointer(2, GL_DOUBLE, 0, &m_textureCoords[0]);
 	}
-	if(!m_faces.empty())
-	{
-#ifdef NEW_RENDERING //new rendering with normals and texcoords. Slow
-		glBegin(GL_TRIANGLES); 
-		for(unsigned int i = 0; i < m_faces.size(); i += 3)
-		{
-			glTexCoord2d(m_vertices[m_faces[i + 1] * 2], m_vertices[m_faces[i + 1] * 2 + 1]);
-			glNormal3d(m_vertices[m_faces[i + 2] * 3], m_vertices[m_faces[i + 2] * 3 + 1], m_vertices[m_faces[i + 2] * 3 + 2]);
-			glVertex3d(m_vertices[m_faces[i] * 3], m_vertices[m_faces[i] * 3 + 1], m_vertices[m_faces[i] * 3 + 2]);
-		}
-		glEnd();
-#else//Old rendering with incorrect normals and texcoords. Very fast.
-		glDrawElements(GL_TRIANGLES, m_faces.size(), GL_UNSIGNED_INT, &m_faces[0]);
-#endif
-	}
-	else 
-	{
-		glDrawArrays(GL_TRIANGLES, 0, m_vertices.size() / 3);
-	}
+	glDrawArrays(GL_TRIANGLES, 0, m_vertices.size() / 3);
 	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 	glDisableClientState(GL_NORMAL_ARRAY);
 	glDisableClientState(GL_VERTEX_ARRAY);
