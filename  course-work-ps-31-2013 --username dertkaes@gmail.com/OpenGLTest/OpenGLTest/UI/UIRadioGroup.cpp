c0fb9f0e2acaf9d4c6dd6e3af70bcb9a9b5d441e
Warbossrus26@gmail.com <Warbossrus26@gmail.com@bb2aebea-ba2d-cad3-75da-7cf554a62b86>
2013-12-16 16:51:49 +0000

    #43 Missing files

---
+++  course-work-ps-31-2013 --username dertkaes@gmail.com/OpenGLTest/OpenGLTest/UI/UIRadioGroup.cpp
@@ -0,0 +1,88 @@
+#include "UIRadioGroup.h"
+#include <GL\glut.h>
+#include "..\view\TextureManager.h"
+#include "UIText.h"
+
+void CUIRadioGroup::Draw() const
+{
+	if(!m_visible)
+		return;
+	glPushMatrix();
+	glTranslatef(GetX(), GetY(), 0);
+	for(size_t i = 0; i < m_items.size(); ++i)
+	{
+		CTextureManager::GetInstance()->SetTexture(m_theme.texture);
+		float y = m_theme.radiogroup.elementSize * i + (m_theme.radiogroup.elementSize - m_theme.radiogroup.buttonSize) / 2;
+		glBegin(GL_TRIANGLE_STRIP);
+		(i == m_selected)?glTexCoord2f(m_theme.radiogroup.selectedTexCoord[0], m_theme.radiogroup.selectedTexCoord[1]):glTexCoord2f(m_theme.radiogroup.texCoord[0], m_theme.radiogroup.texCoord[1]);
+		glVertex2f(0.0f, y);
+		(i == m_selected)?glTexCoord2f(m_theme.radiogroup.selectedTexCoord[0], m_theme.radiogroup.selectedTexCoord[3]):glTexCoord2f(m_theme.radiogroup.texCoord[0], m_theme.radiogroup.texCoord[3]);
+		glVertex2f(0.0f, y + m_theme.radiogroup.buttonSize);
+		(i == m_selected)?glTexCoord2f(m_theme.radiogroup.selectedTexCoord[2], m_theme.radiogroup.selectedTexCoord[1]):glTexCoord2f(m_theme.radiogroup.texCoord[2], m_theme.radiogroup.texCoord[1]);
+		glVertex2f(m_theme.radiogroup.buttonSize, y);
+		(i == m_selected)?glTexCoord2f(m_theme.radiogroup.selectedTexCoord[2], m_theme.radiogroup.selectedTexCoord[3]):glTexCoord2f(m_theme.radiogroup.texCoord[2], m_theme.radiogroup.texCoord[3]);
+		glVertex2f(m_theme.radiogroup.buttonSize, y + m_theme.radiogroup.buttonSize);
+		glEnd();
+		PrintText(m_theme.radiogroup.buttonSize + 1, m_theme.radiogroup.elementSize * i, GetWidth(), m_theme.radiogroup.elementSize, m_items[i], m_theme.text);
+		glColor3f(1.0f, 1.0f, 1.0f);
+	}
+	CTextureManager::GetInstance()->SetTexture("");
+	CUIElement::Draw();
+	glPopMatrix();
+}
+
+bool CUIRadioGroup::LeftMouseButtonUp(int x, int y)
+{
+	if(!m_visible) return false;
+	if(CUIElement::LeftMouseButtonUp(x, y))
+	{
+		return true;
+	}
+	if(PointIsOnElement(x, y))
+	{
+		unsigned int index = (y - GetY()) / m_theme.radiogroup.elementSize;
+		if(index >= 0 && index < m_items.size()) m_selected = index;
+		if(m_onChange) m_onChange();
+		SetFocus();
+		return true;
+	}
+	return false;
+}
+
+void CUIRadioGroup::AddItem(std::string const& str)
+{
+	m_items.push_back(str);
+	if(m_selected == -1)
+	{
+		m_selected = 0;
+	}
+}
+
+std::string const CUIRadioGroup::GetText() const
+{
+	return m_items[m_selected];
+}
+
+void CUIRadioGroup::SetSelected(size_t index)
+{
+	m_selected = index;
+}
+
+void CUIRadioGroup::DeleteItem(size_t index)
+{
+	m_items.erase(m_items.begin() + index);
+	if(m_selected == index) m_selected--;
+	if(m_selected == -1 && !m_items.empty()) m_selected = 0;
+}
+
+void CUIRadioGroup::SetText(std::string const& text)
+{
+	for(size_t i = 0; i < m_items.size(); ++i)
+	{
+		if(m_items[i] == text)
+		{
+			m_selected = i;
+			return;
+		}
+	}
+}
---
+++  course-work-ps-31-2013 --username dertkaes@gmail.com/OpenGLTest/OpenGLTest/UI/UIRadioGroup.h
@@ -0,0 +1,24 @@
+#include "UIElement.h"
+#include <vector>
+
+class CUIRadioGroup : public CUIElement
+{
+public:
+	CUIRadioGroup(int x, int y, int height, int width, IUIElement * parent): CUIElement(x, y, height, width, parent), m_selected(-1), m_onChange(callback()) { }
+	void Draw() const;
+	bool LeftMouseButtonUp(int x, int y);
+	void AddItem(std::string const& str);
+	void DeleteItem(size_t index);
+	std::string const GetText() const;
+	int GetSelectedIndex() const { return m_selected; }
+	size_t GetItemsCount() const { return m_items.size(); }
+	std::string GetItem(size_t index) const { return m_items[index]; }
+	void ClearItems() { m_items.clear(); m_selected = -1; }
+	void SetSelected(size_t index);
+	void SetText(std::string const& text);
+	void SetOnChangeCallback(callback(onChange)) { m_onChange = onChange; }
+private:
+	std::vector<std::string> m_items;
+	int m_selected;
+	callback(m_onChange);
+};
